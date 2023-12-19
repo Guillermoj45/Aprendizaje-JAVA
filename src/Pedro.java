16a134e8d5c3e7f8ffc21c94479b8889d625c3f1
Guillermo <gremesalgarcia@safareyes.es>
2023-12-19 08:43:54 +0100

    Más pruebas

--- src/Pedro.java
+++ src/Pedro.java
@@ -2,7 +2,15 @@ import static utiles.Utilidades.*;
 public class Pedro {
     public static void main(String[] args) {
         int pedro = 10;
+        Object suma = 23;
+        String suma2 = "20";
+        Object respuesta = (int) suma + Integer.parseInt(suma2);
+        String pedro2 = "pito";
         String hol = " hola buenas tardes como te va la vida";
-        print("hola buenas tades " + pedro + hol);
+        print("hola buenas tades %s %s %s", pedro, pedro2, hol);
+        int hola = 12;
+        int hola1= 11;
+        float hola2 = (float) hola/hola1;
+        print(hola2);
     }
 }
--- src/utiles/Utilidades.java
+++ src/utiles/Utilidades.java
@@ -1,7 +1,14 @@
 package utiles;
 
 public class Utilidades {
-    public static void print (Object texto) {
-        System.out.println(texto);
+//    public static void print (Object texto) {
+//        System.out.println(texto);
+//    }
+    public static void print (Object texto, Object... variables) {
+        if (variables != null && variables.length > 0) {
+            System.out.printf((texto.toString()) + "%n", variables);
+        } else {
+            System.out.println(texto);
+        }
     }
 }
