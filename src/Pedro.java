f3f6dc41ae8b15a55c2c2ef5e526495162af0f41
Guillermo <gremesalgarcia@safareyes.es>
2023-12-21 16:28:08 +0100

    añadimos inpuntfloat para los numeros

--- src/Pedro.java
+++ src/Pedro.java
@@ -5,7 +5,7 @@ public class Pedro {
         Object suma = 23;
         String suma2 = "20";
         Object respuesta = (int) suma + Integer.parseInt(suma2);
-        String pedro2 = "pito";
+        String pedro2 = "Pedro2";
         String hol = " hola buenas tardes como te va la vida";
         print("hola buenas tades %s %s %s", pedro, pedro2, hol);
         int hola = 12;
@@ -13,7 +13,17 @@ public class Pedro {
         Object hola2 = (float) hola/hola1;
         Object hola12 = "hola buenas tardes";
         print(hola2);
-        String json = (String) input("Escriba aqui lo que quieres que se repita: ");
+        int hola21 = Integer.parseInt("22");
+        int json =  Integer.parseInt((String) input("Escriba aqui lo que quieres que se repita: "));
         print(json);
+        int json2 = (int) inputfloat("hola buenas tardes: ");
+        print(json2);
+        int b = 0;
+        for(int a = 0; 100000000 >= a; a = a+1)
+        {
+            print("%s", a);
+            b = 99999999 + b;
+            print(b);
+        }
     }
 }
--- src/utiles/Utilidades.java
+++ src/utiles/Utilidades.java
@@ -1,19 +1,26 @@
 package utiles;
 import java.util.Scanner;
 public class Utilidades {
-//    public static void print (Object texto) {
+    //    public static void print (Object texto) {
 //        System.out.println(texto);
 //    }
-    public static void print (Object texto, Object... variables) {
+    public static void print(Object texto, Object... variables) {
         if (variables != null && variables.length > 0) {
             System.out.printf((texto.toString()) + "%n", variables);
         } else {
             System.out.println(texto);
         }
     }
-    public static Object input (Object datos_user) {
+
+    public static Object input(String datos_user) {
         System.out.print(datos_user);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
+    }
+
+    public static double inputfloat(String datos_user) {
+        System.out.print(datos_user);
+        Scanner scanner = new Scanner(System.in);
+        return Float.parseFloat(scanner.nextLine());
     }
 }
